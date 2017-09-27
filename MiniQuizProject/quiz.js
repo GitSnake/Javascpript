5023e80fb43f78d5955ff9ed8fe125e22a2fe7da
CodeVyper <aaron.kiewe@gmail.com>
2017-09-27 01:12:41 -0400

    Inputting code outline

--- MiniQuizProject/quiz.js
+++ MiniQuizProject/quiz.js
@@ -2,3 +2,58 @@
 function print(message) {
   document.write(message);
 }
+
+
+// It will ask a series of questions and evaluate each answer.
+
+
+// The program should keep track of the number of questions answered correctly.
+
+
+// After all the questions are answered the program should display the number of questions that were correctly answered.
+
+
+// And the number that the player got wrong.
+
+
+// You'll use a few concepts from this course to get this done.
+
+
+// First a two dimensional array.
+
+
+// You should use a two dimensional array to hold the questions and the answers.
+
+// Each element of the array represents one question and is itself an array composed of two elements. The question and the answer.
+
+
+// Create a two dimensional array with at least three questions in it.
+
+
+// You'll also use a loop.
+
+
+// You'll need to use a loop to cycle through each question.
+
+
+// Ask it, and compare the response from the player to the answer in the array.
+
+
+// You can use the prompt method to ask the question.
+
+
+// See the teacher's notes if you're not sure how the prompt method works.
+
+
+// You'll then use a conditional statement to see if the player's answer
+
+
+// matches the real answer.
+
+
+// When the loop is done, you should know how many questions were correctly answered.
+
+
+// So print that out to the screen.
+
+
