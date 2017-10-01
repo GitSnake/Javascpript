de36590f382e709ff72739fa02f44dd1936ef80a
CodeVyper <aaron.kiewe@gmail.com>
2017-09-30 22:39:37 -0400

    uncommented line

--- template-literals/js/interpolation.js
+++ template-literals/js/interpolation.js
@@ -4,4 +4,4 @@ function like(thing) {
 
 const sentence = `<p>` + like(`apples`) + `.</p>`;
 
-//document.querySelector('.interpolation').innerHTML = sentence;
+document.querySelector('.interpolation').innerHTML = sentence;
