7a1b1f90ce3ed9ac286b015f859d5b6c83ec5e75
CodeVyper <aaron.kiewe@gmail.com>
2017-09-30 22:49:14 -0400

    Replaced functions with parenthetical placeholders

--- template-literals/js/interpolation.js
+++ template-literals/js/interpolation.js
@@ -6,6 +6,6 @@ function love(thing) {
   return `I like ${thing}`;
 }
 
-const sentence = `<p>` + like(`apples`) + ', but ' + love ('oranges')' + `.</p>`;
+const sentence = `<p>${}, but ${}.</p>`;
 
 document.querySelector('.interpolation').innerHTML = sentence;
