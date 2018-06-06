cb53dfbb5d907f6311f1f006ec44bdcb596acdde
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-06-06 04:37:16 -0400

    Added variables

--- code-generation-javascript-games-maker-advanced/ghost-maze/javascript.js
+++ code-generation-javascript-games-maker-advanced/ghost-maze/javascript.js
@@ -1,3 +1,5 @@
+
+/* mazeData array */
 var mazeData=[
 [1,1,1,1,1,1,1,1]
 [0,0,1,1,0,0,0,1]
@@ -7,3 +9,9 @@ var mazeData=[
 [1,0,0,0,0,1,0,0]
 [1,1,1,1,1,1,1,1]
 ];
+
+/* game variables */
+
+onkeydow=handleKeys;
+onready=drawMaze();
+var px=0, py=1;
