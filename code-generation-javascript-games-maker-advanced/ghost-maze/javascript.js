b7dea014cd018c6e2513d90a5162d5dd5c11778d
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-06-06 06:49:56 -0400

    Drew the maze

--- code-generation-javascript-games-maker-advanced/ghost-maze/javascript.js
+++ code-generation-javascript-games-maker-advanced/ghost-maze/javascript.js
@@ -18,5 +18,18 @@ var px=0, py=1;
 
 /* functions */
 function setLeft(id,x){document.getElementById(id).style.left=x+"px";}
-
 function setTop(id,y){document.getElementById(id).style.top=y+"px";}
+function drawMaze(){for(var y=0; y<7; y++){
+	for(var x=0;x<8; x++){
+		var newOb=document.createElement('div');
+		document.getElementById("mazeHolder").appendChild(newOb);
+		var divId=Math.random();
+		newOb.setAttribute('id',divId);
+		setLeft(divId,50*x);
+		setTop(divId,50*y);
+		if(mazeData[y][x]==0){
+			document.getElementById(divId).style.backgroundColor="blue";
+			}
+		}
+	}
+}
