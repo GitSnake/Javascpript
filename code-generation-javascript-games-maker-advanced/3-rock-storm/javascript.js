c9161cc7a7942f3baafd89a866d89e038f4f8ad8
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-06-14 16:07:31 -0400

    Added game functions

--- code-generation-javascript-games-maker-advanced/3-rock-storm/javascript.js
+++ code-generation-javascript-games-maker-advanced/3-rock-storm/javascript.js
@@ -5,4 +5,8 @@ document.onekeydown=handleKeys;
 onready=startUp();
 
 /* functions */
-function setLeft(id,x){}
+function setLeft(id,x){document.getElementById(id).style.left=x+"px";}
+function setTop(id,y){document.getElementById(id).style.top=y+"px";}
+function getLeft(id){return document.getElementById(id).offsetLeft;}
+function getLeft(id){return document.getElementById(id)offsetTop;}
+function randomNumber(low,high){return(Math.floor(low+Math.random()*(1+high-low)));}
