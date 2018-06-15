69cc58695ad2abcc73840b523503b8f25cb92117
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-06-14 21:05:55 -0400

    Bug fixing

--- code-generation-javascript-games-maker-advanced/3-rock-storm/javascript.js
+++ code-generation-javascript-games-maker-advanced/3-rock-storm/javascript.js
@@ -8,16 +8,13 @@ onready=startUp();
 function setLeft(id,x){document.getElementById(id).style.left=x+"px";}
 function setTop(id,y){document.getElementById(id).style.top=y+"px";}
 function getLeft(id){return document.getElementById(id).offsetLeft;}
-function getLeft(id){return document.getElementById(id)offsetTop;}
+function getTop(id){return document.getElementById(id)offsetTop;}
 function randomNumber(low,high){return(Math.floor(low+Math.random()*(1+high-low)));}
 
 /* overlapping test function */
 function isOverlapping(ob1,ob2){
-	return ((getLeft(ob1)+100>getLeft(ob2))
-&& (getLeft(ob1)<getLeft(ob2)+100) &&
-(getTop(ob1)+100>getTop(ob2)) &&
-(getTop(ob1)<getTop(ob2)+100));
-}
+	return ((getLeft(ob1)+100>getLeft(ob2)) && (getLeft(ob1)<getLeft(ob2)+100) && (getTop(ob1)+100>getTop(ob2)) &&
+(getTop(ob1)<getTop(ob2)+100));}
 
 /* startup function */
 function startUp(){
